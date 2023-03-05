084ad8cd8f082d140c121cd87144c25c37e6f09b
suryamodulus <suryamodulus@gmail.com>
2023-03-05 17:00:34 +0530

    add sqlite db to gitignore

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { ProjectsApiController } from './projects.api.controller';
+import { ProjectsWebController } from './projects.web.controller';
 
 @Module({
-  controllers: [ProjectsApiController],
+  controllers: [ProjectsApiController, ProjectsWebController],
   providers: [ProjectsService],
 })
 export class ProjectsModule {}
